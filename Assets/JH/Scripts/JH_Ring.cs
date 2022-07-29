3f9c82cd4aace7b8c42890c2170353dfc7ae352e
Kang-JiHyeon <65221707+Kang-JiHyeon@users.noreply.github.com>
2022-07-29 16:45:19 +0900

    Enemy

---
+++ Assets/JH/Scripts/JH_Ring.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 1. 일반
+// 플레이어와 닿이면 사라지게 하고 싶다.
+
+// 2. 부스터
+// 플레이어와 일정 거리 안에 있으면 플레이어 쪽으로 이동하고 싶다.
+public class JH_Ring : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
