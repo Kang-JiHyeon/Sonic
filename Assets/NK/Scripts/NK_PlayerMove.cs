a8b1e9e5c2d376b24c9f7b9b453da08fdb7c0057
kimnakyung <35446851+dd6679@users.noreply.github.com>
2022-08-04 17:20:13 +0900

    .

--- Assets/NK/Scripts/NK_PlayerMove.cs
+++ Assets/NK/Scripts/NK_PlayerMove.cs
@@ -22,6 +22,7 @@ public class NK_PlayerMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         cc = GetComponent<CharacterController>();
     }
 
