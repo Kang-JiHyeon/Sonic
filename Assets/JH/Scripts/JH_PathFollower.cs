5a9f2e89f70465df6e9e80a9e3259b5beada2a08
Kang-JiHyeon <65221707+Kang-JiHyeon@users.noreply.github.com>
2022-08-09 19:02:36 +0900

    두번째 롤러코스터 카메라

--- Assets/JH/Scripts/JH_PathFollower.cs
+++ Assets/JH/Scripts/JH_PathFollower.cs
@@ -4,57 +4,74 @@ using UnityEngine;
 public class JH_PathFollower : MonoBehaviour
 {
 
-	public float speed = 10f;
-	public Transform pathParent;
+	public float[] speed;
+	public List<Transform> pathList;
+	//public Transform pathParent;
 	public Transform player;
 	Transform targetPoint;
-	int index;
-	bool isCameraMove = false;
+	int childIndex;
+	int listIndex;
+	public bool isCameraMove = false;
 
 	void OnDrawGizmos()
 	{
 		Vector3 from;
 		Vector3 to;
-		for (int a = 0; a < pathParent.childCount - 1; a++)
-		{
-			from = pathParent.GetChild(a).position;
-			to = pathParent.GetChild((a + 1)).position;
-			Gizmos.color = new Color(1, 0, 0);
-			Gizmos.DrawLine(from, to);
-		}
+
+		for(int i = 0; i< pathList.Count; i++)
+        {
+			for (int j = 0; j < pathList[i].childCount - 1; j++)
+			{
+				from = pathList[i].GetChild(j).position;
+				to = pathList[i].GetChild((j + 1)).position;
+				Gizmos.color = new Color(1, 0, 0);
+				Gizmos.DrawLine(from, to);
+			}
+
+        }
 	}
 	void Start()
 	{
-		index = 0;
-		targetPoint = pathParent.GetChild(index);
+		childIndex = 0;
+		listIndex = 0;
+		targetPoint = pathList[listIndex].GetChild(childIndex);
+		speed = new float[pathList.Count];
+		speed[0] = 30f;
+		speed[1] = 30f;
+
+
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if(index >= pathParent.childCount -1)
+		if(childIndex >= pathList[listIndex].childCount - 1)
         {
 			isCameraMove = false;
-			index = 0;
-        }
+			childIndex = 0;
+			listIndex++;
+			listIndex %= pathList.Count;
+			targetPoint = pathList[listIndex].GetChild(childIndex);
+		}
 
 		// 커브
         if (isCameraMove)
         {
-			transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, speed * Time.deltaTime);
+			transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, speed[listIndex] * Time.deltaTime);
 			transform.LookAt(player);
 
 			if (Vector3.Distance(transform.position, targetPoint.position) < 0.1f)
 			{
-				index++;
-                targetPoint = pathParent.GetChild(index);
+				childIndex++;
+                targetPoint = pathList[listIndex].GetChild(childIndex);
 			}
         }
 		// 직선
         else
         {
-			transform.position = Vector3.Lerp(transform.position, player.position, speed * Time.deltaTime);
-        }
+			transform.position = Vector3.Lerp(transform.position, player.position, speed[listIndex] * Time.deltaTime);
+			childIndex = 0;
+		}
 	}
     private void OnTriggerEnter(Collider other)
     {
