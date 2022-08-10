cb1949f4589530f09e021b06c42dba64b35f36ce
Kang-JiHyeon <65221707+Kang-JiHyeon@users.noreply.github.com>
2022-08-10 09:43:46 +0900

    Camera Field of view

--- Assets/JH/Scripts/JH_JumpBlock.cs
+++ Assets/JH/Scripts/JH_JumpBlock.cs
@@ -6,25 +6,25 @@ using UnityEngine;
 // 필요속성: 플레이어, 플레이어의 yVelocity
 public class JH_JumpBlock : MonoBehaviour
 {
-    public float originJumpSpeed;
-    float jumpBlockPower = 20f;
+
+    float originJumpPower = 15f;
+    public float jumpBlockPower = 20f;
 
     private void Start()
     {
-        originJumpSpeed = NK_PlayerMove.Instance.jumpSpeed;
+        //originJumpPower = NK_PlayerMove.Instance.jumpPower;
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.name.Contains("Player"))
         {
             NK_PlayerMove.Instance.isJumpBlock = true;
-            NK_PlayerMove.Instance.jumpSpeed = jumpBlockPower;
-            //print(JH_PlayerMove.Instance.jumpSpeed); 
+            NK_PlayerMove.Instance.jumpPower = jumpBlockPower;
         }
     }
     private void OnTriggerExit(Collider other)
     {
         NK_PlayerMove.Instance.isJumpBlock = false;
-        NK_PlayerMove.Instance.jumpSpeed = originJumpSpeed;
+        NK_PlayerMove.Instance.jumpPower = originJumpPower;
     }
 }
