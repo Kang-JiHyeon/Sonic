0aae7b2fd0b521cc1067ca088bd7584a51f50745
Kang-JiHyeon <65221707+Kang-JiHyeon@users.noreply.github.com>
2022-08-18 13:56:49 +0900

    벌 에너미/떨어지는 링

--- Assets/NK/Scripts/NK_Attack.cs
+++ Assets/NK/Scripts/NK_Attack.cs
@@ -15,7 +15,7 @@ public class NK_Attack : MonoBehaviour
     float currentTime = 0;
     float shortDistance;
     bool isAiming = false;
-    bool isAttack = false;
+    public bool isAttack = false;
     GameObject aim;
 
     public static NK_Attack Instance;
