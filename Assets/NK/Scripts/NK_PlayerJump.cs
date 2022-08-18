8fc6e553d5b94e8c9c9351afb4b14fc78cba95e6
kimnakyung <35446851+dd6679@users.noreply.github.com>
2022-08-18 13:34:40 +0900

    Revert "."
    This reverts commit 477ea0392d8cbf4d840e77acd62c57261de85747.

--- Assets/NK/Scripts/NK_PlayerJump.cs
+++ Assets/NK/Scripts/NK_PlayerJump.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NK_PlayerJump : MonoBehaviour
 {
-    /*NK_PlayerJump playerJump;
+    NK_PlayerJump playerJump;
     float jumpSpeed;
 
     void Start()
@@ -26,80 +26,6 @@ public class NK_PlayerJump : MonoBehaviour
             transform.localEulerAngles = Vector3.zero;
             transform.root.localEulerAngles = Vector3.zero;
             playerJump.enabled = false;
-        }
-    }*/
-
-    public TrailRenderer trailRenderer;
-    public float jumpSpeed = 10.0f;
-    public float jumpPower = 3;
-    public bool isJumping;
-    public bool isJumpBlock;
-    public Vector3 dir = Vector3.zero;
-
-    Animator anim;
-    CharacterController controller;
-    float jumpTime;
-
-    public static NK_PlayerJump Instance;
-
-    private void Awake()
-    {
-        Instance = this;
-    }
-
-    private void Start()
-    {
-        isJumping = false;
-        jumpTime = 0;
-        controller = GetComponent<CharacterController>();
-        anim = GetComponentInChildren<Animator>();
-    }
-
-    private void Update()
-    {
-        if (controller.isGrounded)
-        {
-            // 점프블럭 전 트레일렌더러 비활성화
-            trailRenderer.enabled = false;
-
-            anim.SetBool("IsJumping", false);
-            anim.SetBool("IsSpringJumping", false);
-            isJumping = false;
-
-            if ((Input.GetButton("Jump") || isJumpBlock) && !isJumping)
-            {
-                if (isJumpBlock)
-                {
-                    anim.SetBool("IsSpringJumping", true);
-                    trailRenderer.enabled = true;
-                }
-                else
-                {
-                    anim.SetBool("IsJumping", true);
-                }
-
-                isJumping = true;
-            }
-
-            if (isJumping)
-            {
-                Jump();
-            }
-        }
-        controller.Move(dir * jumpPower * Time.deltaTime);
-    }
-
-    public void Jump()
-    {
-        float height = (jumpTime * jumpTime * (-NK_PlayerMove.Instance.gravity) / 2) + (jumpTime * jumpPower);
-
-        dir.y = jumpSpeed + height;
-        jumpTime += Time.deltaTime;
-
-        if (height < 0.0f)
-        {
-            isJumping = false;
-            jumpTime = 0.0f;
         }
     }
 }
--- Assets/NK/Scripts/NK_PlayerMove.cs
+++ Assets/NK/Scripts/NK_PlayerMove.cs
@@ -14,7 +14,9 @@ public class NK_PlayerMove : MonoBehaviour
     public Vector3 dir = Vector3.zero;
 
     Vector3 look = Vector3.forward;
+    Vector3 camDir;
     CharacterController controller;
+    NK_PlayerJump playerJump;
     Animator anim;
     float jumpTime;
 
@@ -31,6 +33,7 @@ public class NK_PlayerMove : MonoBehaviour
         jumpTime = 0;
         transform.localEulerAngles = Vector3.zero;
         controller = GetComponent<CharacterController>();
+        trailRenderer = GetComponentInChildren<TrailRenderer>();
         /*GameObject player = transform.GetChild(0).gameObject;
         playerJump = player.GetComponent<NK_PlayerJump>();*/
         anim = GetComponentInChildren<Animator>();
@@ -110,7 +113,6 @@ public class NK_PlayerMove : MonoBehaviour
         float height = (jumpTime * jumpTime * (-gravity) / 2) + (jumpTime * jumpPower);
 
         dir.y = jumpSpeed + height;
-        dir += transform.forward * 5;
         jumpTime += Time.deltaTime;
 
         if (height < 0.0f)
