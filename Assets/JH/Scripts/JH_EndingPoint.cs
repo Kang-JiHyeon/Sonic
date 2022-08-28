cf141b895653c677522cabbb5f0d48db9140b755
Kang-JiHyeon <65221707+Kang-JiHyeon@users.noreply.github.com>
2022-08-29 03:53:45 +0900

    ending Cinemachine 추가

--- Assets/JH/Scripts/JH_EndingPoint.cs
+++ Assets/JH/Scripts/JH_EndingPoint.cs
@@ -11,19 +11,26 @@ public class JH_EndingPoint : MonoBehaviour
     public Text ring;
     public Text score;
 
+    public JH_CinemachineDirector cine;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name.Contains("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
             JH_SoundManager.Instance.PlaySound("Ending");
-            //GameManager.gameManager.m_state = GameManager.GameState.GameOver;
 
             // 링 개수, 플레이 시간, 점수 저장
             PlayerPrefs.SetString("PlayTime", playTime.text);
             PlayerPrefs.SetString("Ring", ring.text);
             PlayerPrefs.SetString("SumScore", score.text);
 
-            GameManager.gameManager.m_state = GameManager.GameState.GameOver;
+            // 달리는 player 비활성화
+            other.gameObject.SetActive(false);
+
+            // 시네머신 실행
+            cine.camera.enabled = true;
+            cine.isPlay = true;
+            cine.pd.Play();
         }
     }
 }
