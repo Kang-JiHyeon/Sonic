bfdfb54017ecc7efd4d74f7a807abcc1f0a52341
kimnakyung <35446851+dd6679@users.noreply.github.com>
2022-08-29 03:35:53 +0900

    update score

--- Assets/NK/Scripts/NK_TopScoreUI.cs
+++ Assets/NK/Scripts/NK_TopScoreUI.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NK_TopScoreUI : MonoBehaviour
 {
+    public GameObject topScore;
+    public GameObject topPlayTime;
+    public GameObject topRing;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,11 @@ public class NK_TopScoreUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var ts = PlayerPrefs.GetInt("TopScore");
+        var ss = Int32.Parse(PlayerPrefs.GetString("SumScore"));
+        if (PlayerPrefs.GetInt("TopScore") <= Int32.Parse(PlayerPrefs.GetString("SumScore")))
+        {
+            topScore.SetActive(true);
+        }
     }
 }
